31d357b20785426d4a85f71e2ba7ce02584c6625
ericm <ericm@MUGANGA>
2022-11-29 22:09:57 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿Char a, b, c, d, e, f, g, h, j;
+
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
+Console.WriteLine();
+
+Console.Write("What\'s the state of the first field: ");
+a = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+b = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+c = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+d = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+e = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+f = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+g = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+h = Convert.ToChar(Console.ReadLine());
+
+Console.Write("What\'s the state of the first field: ");
+j = Convert.ToChar(Console.ReadLine());
+
+
+Console.WriteLine($" {a} | {b} | {c} ");
+Console.WriteLine("---+---+---");
+Console.WriteLine($" {d} | {e} | {f} ");
+Console.WriteLine("---+---+---");
+Console.WriteLine($" {g} | {h} | {j} ");
